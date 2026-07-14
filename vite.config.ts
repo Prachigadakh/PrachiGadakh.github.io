ec575eb60134f66b60adbc55d31e3ae659e6f05f
prachigadakh <prachipgadakh2003@gmail.com>
2026-07-14 22:05:29 +0530

    update vite file

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    watch: {
-      ignored: ['**/.vs/**']
-    }
-  }
+  base: '/'
 })
